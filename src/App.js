908a1aca9effba551454c7e006cfe3dbdbf8ce6a
harshtyagimdr <harsh.17bcs1046@abes.ac.in>
2020-04-19 10:39:07 +0530

    react movie

--- src/App.js
+++ src/App.js
@@ -1,26 +1,71 @@
 import React from 'react';
-import logo from './logo.svg';
+import axios from 'axios'
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+class App extends React.Component {
+  constructor(props){
+    super(props);
+    this.state={
+      movies:[],
+      
+    };
+    this.onSubmit = this.onSubmit.bind(this);
+  }
+  componentDidMount(query){
+    
+    var url="http://www.omdbapi.com/?i=tt3896198&apikey=242f041&s=";
+    axios.get(url+`${query?query:"harry"}`).then(res=>{
+      this.setState({
+        movies:res.data.Search
+      })
+    })
+  }
+  onSubmit(event){
+    event.preventDefault();
+    var query = this.input.value;
+    // console.log(query);
+    this.componentDidMount(query);
+  }
+  render(){
+   
+    const {movies} = this.state;
+    console.log(movies)
+    if(movies!==undefined){
+    var movieList = movies.map((movie) => 
+    
+    <div className="col-4 movie">  
+      <img src={movie.Poster!=="N/A"?movie.Poster:"https://i.imgur.com/L6a1AjW.png"}className="movieImg" />
+      <p className="overview">{movie.Title}</p>
+      <h3  key={movie.imdbID} className="text-center movieTitle">{movie.Year}</h3>
     </div>
-  );
+    )
+    }
+    else{
+      var movieList=<div className="alert alert-danger my-5">No movie </div>
+    }
+   
+    return (
+      <div className="App">
+        <div className="jumbotron">  
+            <div className="container">
+            <div className="row">
+            <h2 className="col-12 text-center">Search for a Movie</h2>
+            <span className=" col-12 text-center"><b>Created By Harsh tyagi</b></span>
+              <form onSubmit={this.onSubmit} className="col-12">
+                <input className= "col-12 form-control" placeholder="Search Movies..."
+                ref = {input => this.input = input}/>
+              </form>
+              <div>
+                <ul className= "col-12 row">{movieList}</ul>
+              </div>
+              </div>
+            </div>
+          </div>
+      </div>
+    );
+  }
+  
+  
 }
 
 export default App;
